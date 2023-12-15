56946b42ea7c6c01f24f15e4231c8e69fb368c21
Yash Hingane <hingane_y@es.iitr.ac.in>
2023-12-15 18:50:14 +0530

    added calculate winner functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,23 +4,157 @@ import Square from "./Square.js"
 
 export default function Board(){
     const [turn, setTurn] = React.useState(true);
-    const tempArr = [1 , 2 ,3];
-
+    const tempArr = [1 , 2 , 3];
+    const [arr , setArr] = React.useState(tempArr.map(item => (new Array(3).fill(" "))));
+    const [winMsg , setWinMsg] = React.useState(0);
     function toggleTurn(){
+        const winner = calculateWinner(arr);
+        console.log(winner)
+        if(winner !== 0){
+            if(turn) setWinMsg(1);
+            else setWinMsg(2);
+            return;
+        }
         setTurn(!turn);
     }
 
+    console.log(arr);
+
+    
     return (
         <>
+            {winMsg === 0 && <h1>{ `${turn ? "First" : "Second"} Player's turn` }</h1>}
+            {winMsg !== 0 && <h1>{`Player ${winMsg === 1 ? "First" : "Second"} is the Winner`}</h1>}
             <div className="board-row">
-                {tempArr.map(item => (<Square turn = {turn} toggleTurn = {toggleTurn}/>) )}
+                {tempArr.map(item => (
+                    <Square 
+                        id = {item}
+                        x = {0}
+                        y = {item - 1}
+                        arr = {arr}
+                        setArr = {setArr}
+                        turn = {turn} 
+                        toggleTurn = {toggleTurn}
+                    />
+                ) )}
             </div>
+
             <div className="board-row">
-                {tempArr.map(item => (<Square turn = {turn} toggleTurn = {toggleTurn}/>) )}
+                {tempArr.map(item => (
+                    <Square 
+                        id = {3 + item}
+                        x = {1}
+                        y = {item - 1}
+                        arr = {arr}
+                        setArr = {setArr}
+                        turn = {turn} 
+                        toggleTurn = {toggleTurn}
+                    />
+                ) )}
             </div>
+
             <div className="board-row">
-                {tempArr.map(item => (<Square turn = {turn} toggleTurn = {toggleTurn}/>) )}
+                {tempArr.map(item => (
+                    <Square 
+                        id = {6 + item}
+                        x = {2}
+                        y = {item - 1}
+                        arr = {arr}
+                        setArr = {setArr}
+                        turn = {turn}
+                        toggleTurn = {toggleTurn}
+                    />
+                ) )}
             </div>
         </>
     );
+}
+
+function calculateWinner(arr){
+    for(let j = 0 ; j < 3 ; j++){
+        let check = true;
+        // check column for X
+        for(let i = 0 ; i < 3 ; i++){
+            if(arr[i][j] !== 'X'){
+                check = false;
+                break;
+            }
+        }
+        if(check) return 1;
+        
+        // check column for O
+        check = true;
+        for(let i = 0 ; i < 3 ; i++){
+            if(arr[i][j] !== 'O'){
+                check = false;
+                break;
+            }
+        }
+        if(check) return 2;
+    
+    }
+    
+    for(let  i = 0 ; i < 3 ; i++){
+        let check = true;
+        // check row for X
+        for(let j = 0 ; j < 3 ; j++){
+            if(arr[i][j] !== "X"){
+                check = false;
+                break;
+            }
+        }
+        if(check) return 1;
+        
+        // check row for O
+        check = true;
+        for(let j = 0 ; j < 3 ; j++){
+            if(arr[i][j] !== "O"){
+                check = false;
+                break;
+            }
+        }
+        if(check) return 2;
+        
+    }
+    
+    
+    //diagonal check for winner
+    let check = true;
+    for(let i = 0; i < 3 ; i++){
+        if(arr[i][i] !== "X"){
+            check = false;
+            break;
+        }
+    }
+    if(check) return 1;
+
+    check = true;
+    for(let i = 0; i < 3 ; i++){
+        if(arr[i][i] !== "O"){
+            check = false;
+            break;
+        }
+    }
+    if(check) return 2;
+
+    //reverse diagonal check for winner
+    check = true;
+    for(let i = 0; i < 3 ; i++){
+        if(arr[i][2-i] !== "X"){
+            check = false;
+            break;
+        }
+    }
+    if(check) return 1;
+
+    check = true;
+    for(let i = 0; i < 3 ; i++){
+        if(arr[i][2-i] !== "O"){
+            check = false;
+            break;
+        }
+    }
+    if(check) return 2;
+
+    return 0;
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -8,8 +8,16 @@ export default function Square(props){
         if(val === " "){
             if(props.turn) setVal("X");
             else setVal("O");
+
+            const newArr = [...props.arr];
+            if(props.turn) newArr[props.x][props.y] = "X";
+            else newArr[props.x][props.y] = "O";
+            
+            props.setArr(newArr);
+            props.toggleTurn();
+        }else{
+            return;
         }
-        props.toggleTurn();
     }
     return(
         <div className="box" onClick={toggleBox}>{val}</div>
